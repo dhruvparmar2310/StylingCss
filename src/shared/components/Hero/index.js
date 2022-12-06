9fac080b9ebd7911cab41c5f2600dc9b629294b1
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2022-12-06 11:37:18 +0530

    Add files via upload

---
+++ src/shared/components/Hero/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Hero/style.css'
+
+export default function Hero () {
+  return (
+    <>
+        <section className='hero'>
+            <div className='inner-content'>
+                <h1>Agency.</h1>
+                <p>There are many variations of passages but the majority have suffered alteration.</p>
+                <button>Contact Us</button>
+            </div>
+        </section>
+    </>
+  )
+}
---
+++ src/shared/components/Statistics/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Statistics () {
+  return (
+    <>
+        <div className='statistics-card'>
+            <h1>992<sup>+</sup></h1>
+            <p>The standard chunk of Lorem Ipsum used since the 1500s is reproducted below for those.</p>
+        </div>
+    </>
+  )
+}
